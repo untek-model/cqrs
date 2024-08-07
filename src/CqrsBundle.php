165d0dbda0504ef78416dc60f2554c17adcf1573
Vitaliy <theyamshikov@yandex.ru>
2024-08-07 20:13:43 +0500

    upd

---
+++ src/CqrsBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Untek\Model\Cqrs;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Untek\Model\Cqrs\Application\Abstract\CqrsHandlerInterface;
+
+class CqrsBundle extends AbstractBundle
+{
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $builder->registerForAutoconfiguration(CqrsHandlerInterface::class)
+            ->addTag('cqrs.handler');
+    }
+}
