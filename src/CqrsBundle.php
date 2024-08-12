ae0c829bac98fc8131becb2768a05c7f7a084fb2
Vitaliy <theyamshikov@yandex.ru>
2024-08-12 18:01:42 +0500

    upd

--- src/CqrsBundle.php
+++ src/CqrsBundle.php
@@ -2,8 +2,10 @@
 
 namespace Untek\Model\Cqrs;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Untek\Model\Cqrs\Application\Abstract\CqrsHandlerInterface;
 use Untek\Model\Cqrs\Infrastructure\DependencyInjection\CqrsExtension;
@@ -14,15 +16,16 @@ class CqrsBundle extends AbstractBundle
 
     public function build(ContainerBuilder $container)
     {
-        parent::build($container);
-
         $container->addCompilerPass(new CqrsPass());
+
+        $fileLocator = new FileLocator(__DIR__);
+        $loader = new PhpFileLoader($container, $fileLocator);
+        $loader->load(__DIR__ . '/resources/config/services/main.php');
     }
 
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $builder->registerForAutoconfiguration(CqrsHandlerInterface::class)
             ->addTag('cqrs.handler');
-        $container->import(__DIR__ . '/resources/config/services/main.php');
     }
 }
